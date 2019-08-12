2a9d7a603bae52204b802ffdf4e31c8c1eb45286
vitalij <uryashnik@gmail.com>
2019-08-12 04:40:55 +0300

    add actions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,10 +1,28 @@
-const addTodo = (text) => {
+export const VisibilityFilters = {
+    SHOW_ALL: 'SHOW_ALL',
+    SHOW_COMPLETED: 'SHOW_COMPLETED',
+    SHOW_ACTIVE: 'SHOW_ACTIVE'
+};
+
+
+
+export function addTodo(text){
     return {
         type: ADD_TODO,
         text: text
     }
 };
 
-export {
-    addTodo
+export function toggleTodo(index){
+    return {
+        type: TOGGLE_TODO,
+        index
+    }
+};
+
+export function setVisibilityFilter(filter){
+    return {
+        type: SET_VISIBILITY_FILTER,
+        filter
+    }
 };
