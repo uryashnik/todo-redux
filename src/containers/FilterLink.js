d43eb2117410dbc66dc013c87ddca3fceb96a15b
vitalij <uryashnik@gmail.com>
2019-08-15 06:13:22 +0300

    final

--- src/containers/FilterLink.js
+++ src/containers/FilterLink.js
@@ -4,7 +4,7 @@ import Link from '../components/Link'
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    active: true
+    active: ownProps.filter === state.visibilityFilter
   }
 }
 
